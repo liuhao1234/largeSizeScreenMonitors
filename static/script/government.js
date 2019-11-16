99dd20fed6dd12f0ca0b6f7b00f8a1765ca3aea1
liuhao1234 <728672190@qq.com>
2019-11-16 16:50:54 +0800

    添加滚动条样式

--- static/script/government.js
+++ static/script/government.js
@@ -1,4 +1,5 @@
 $(function(){
+    customScroll(".government-text")
     $.initGdpPieChart({
         domId:"gdpPieChart",
         data:gdpPieData
@@ -19,3 +20,17 @@ $(function(){
         data:opulationRatioData
     })
 })
+
+function customScroll(selector){
+    var bars = '.jspHorizontalBar, .jspVerticalBar';
+    $(selector).bind('jsp-initialised', function (event, isScrollable) {
+        $(this).find(bars).hide();
+    }).jScrollPane().hover(
+        function () {
+            $(this).find(bars).stop().fadeTo('fast', 0.9);
+        },
+        function () {
+            $(this).find(bars).stop().fadeTo('fast', 0);
+        }
+    );	
+}
