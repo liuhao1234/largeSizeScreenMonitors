2951d634554874a77b0f77fd636a0d57295a6f5f
liuhao1234 <728672190@qq.com>
2019-11-19 15:38:48 +0800

    chart轮播

--- static/script/government.js
+++ static/script/government.js
@@ -1,10 +1,10 @@
 $(function(){
     customScroll(".government-text")
-    $.initGdpPieChart({
+    var gdpPieChart = $.initGdpPieChart({
         domId:"gdpPieChart",
         data:gdpPieData
     })
-
+    gdpPieChartCarousel(gdpPieChart);
     $.initGdpLineChart({
         domId:"gdpLineChart",
         data:gdpLineData
@@ -19,7 +19,37 @@ $(function(){
         domId:"opulationRatio",
         data:opulationRatioData
     })
+    
 })
+function gdpPieChartCarousel(gdpPieChart){//chart轮播
+    var index = 0;
+    var $webSellData = $(".web-sell-data");
+    var $webSellDataValue = $webSellData.find("b");
+    var $webSellDataName = $webSellData.find("small");
+    var curData = gdpPieData[index++];
+    $webSellDataValue.text(curData.value)
+    $webSellDataName.text(curData.name)
+    gdpPieChart.dispatchAction({
+        type:"highlight",
+        name:curData.name
+    })
+    setInterval(function(){
+        var curData = gdpPieData[index++];
+        $webSellDataValue.text(curData.value)
+        $webSellDataName.text(curData.name)
+        gdpPieChart.dispatchAction({
+            type: 'downplay',
+            seriesIndex: 0
+        });
+        gdpPieChart.dispatchAction({
+            type:"highlight",
+            name:curData.name
+        })
+        if(index === gdpPieData.length){
+            index = 0
+        }
+    },5000)
+}
 
 function customScroll(selector){
     var bars = '.jspHorizontalBar, .jspVerticalBar';
